8af1fa399cb3f28bf41ecd0bc93cc5a528e41b10
Carb0nMC <carbon@carbonmc.net>
2023-06-07 22:22:00 +0200

    Bugfixes

--- YFabric/src/main/java/io/github/thewebcode/y/gui/LoginScreen.java
+++ YFabric/src/main/java/io/github/thewebcode/y/gui/LoginScreen.java
@@ -5,6 +5,6 @@ import net.minecraft.text.Text;
 
 public class LoginScreen extends CottonClientScreen {
     public LoginScreen(LoginGuiDescription description){
-        super(Text.of("Login to YClient"), description);
+        super(Text.of("Login to YServer"), description);
     }
 }
