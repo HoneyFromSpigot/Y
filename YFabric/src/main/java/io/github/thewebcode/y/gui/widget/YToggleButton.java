39248552a873993290c24cba1dfc9fd24b2e103b
Carb0nMC <carbon@carbonmc.net>
2023-06-13 18:58:37 +0200

    Updated to 1.20.1

--- YFabric/src/main/java/io/github/thewebcode/y/gui/widget/YToggleButton.java
+++ YFabric/src/main/java/io/github/thewebcode/y/gui/widget/YToggleButton.java
@@ -2,10 +2,26 @@ package io.github.thewebcode.y.gui.widget;
 
 import io.github.cottonmc.cotton.gui.client.ScreenDrawing;
 import io.github.cottonmc.cotton.gui.widget.WToggleButton;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.math.MatrixStack;
 
 public class YToggleButton extends WToggleButton {
+
     @Override
+    public void paint(DrawContext context, int x, int y, int mouseX, int mouseY) {
+        ScreenDrawing.texturedRect(context, x, y, 18, 29, isOn ? onImage : offImage, 0xFFFFFFFF);
+        if (isFocused()) {
+            ScreenDrawing.texturedRect(context, x, y, 18, 24, focusImage, 0xFFFFFFFF);
+        }
+
+        if (label!=null) {
+            ScreenDrawing.drawString(context, label.asOrderedText(), x + 22, y+6, shouldRenderInDarkMode() ? darkmodeColor : color);
+        }
+        super.paint(context, x, y, mouseX, mouseY);
+    }
+
+    /*
+    @Deprecated
     public void paint(MatrixStack matrices, int x, int y, int mouseX, int mouseY) {
         ScreenDrawing.texturedRect(matrices, x, y, 18, 29, isOn ? onImage : offImage, 0xFFFFFFFF);
         if (isFocused()) {
@@ -16,4 +32,6 @@ public class YToggleButton extends WToggleButton {
             ScreenDrawing.drawString(matrices, label.asOrderedText(), x + 22, y+6, shouldRenderInDarkMode() ? darkmodeColor : color);
         }
     }
+
+     */
 }
