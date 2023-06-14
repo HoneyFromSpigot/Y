a1a53278529fb88b7d6f7882dfbf7b5a78448454
Carb0nMC <carbon@carbonmc.net>
2023-06-14 16:17:36 +0200

    Changed NMS

--- YPlugin/src/main/java/io/github/thewebcode/yplugin/utils/PacketDataUtil.java
+++ YPlugin/src/main/java/io/github/thewebcode/yplugin/utils/PacketDataUtil.java
@@ -1,15 +1,14 @@
 package io.github.thewebcode.yplugin.utils;
 
 import io.github.thewebcode.yplugin.YPlugin;
+import io.github.thewebcode.yplugin.nms.NMSPlayer;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import net.minecraft.network.PacketDataSerializer;
 import net.minecraft.network.protocol.game.PacketPlayInCustomPayload;
 import net.minecraft.network.protocol.game.PacketPlayOutCustomPayload;
 import net.minecraft.resources.MinecraftKey;
-import net.minecraft.server.network.PlayerConnection;
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import java.nio.charset.StandardCharsets;
@@ -21,49 +20,43 @@ public class PacketDataUtil {
             case "handshake_c2s":
                 String values = readBufAsString(byteBuf);
                 String[] valueAr = values.split("\\|");
-                String player = valueAr[0].replaceAll(" ", "").replaceAll("\\W", "");
+                String playerName = valueAr[0].replaceAll(" ", "").replaceAll("\\W", "");
                 String pass = valueAr[1].replaceAll(" ", "").replaceAll("\\W", "");
                 String masterPass = FileService.get().getConfig().getString("master-password").replace(" ", "");
-
-                Player player1 = Bukkit.getPlayer(player.replaceAll(" ", ""));
+                Player bukkitPlayer = Bukkit.getPlayer(playerName.replaceAll(" ", ""));
 
                 if(masterPass.equalsIgnoreCase(pass)){
-                    if(player1 == null) return;
-
+                    if(bukkitPlayer == null) return;
 
                     boolean needOp = new ServerSettingService.SettingReader<Boolean>().get(ServerSettingService.ServerSetting.OPERATOR_FOR_SETTINGS);
 
-                    if(needOp && !player1.isOp()){
-                        player1.sendMessage("§c§lYMod §r§7» §cYou are not an operator!");
+                    if(needOp && !bukkitPlayer.isOp()){
+                        bukkitPlayer.sendMessage("§c§lYMod §r§7» §cYou are not an operator!");
                         return;
                     }
 
-                    LoggingService.warning(player + " has logged in with the master password! Sending RSK...");
+                    LoggingService.warning(playerName + " has logged in with the master password! Sending RSK...");
 
                     String serverRemoteKey = EncryptionUtil.randomCase(EncryptionUtil.genPassword(20));
-                    YPlugin.getInstance().getRemoteSessionManager().addKey(player1.getName(), serverRemoteKey);
+                    YPlugin.getInstance().getRemoteSessionManager().addKey(bukkitPlayer.getName(), serverRemoteKey);
 
                     ByteBuf buf = Unpooled.buffer();
                     buf.writeBytes(serverRemoteKey.getBytes());
-                    PacketPlayOutCustomPayload payload = new PacketPlayOutCustomPayload(new MinecraftKey("yplugin", "handshake_s2c"), new PacketDataSerializer(buf));
+                    PacketPlayOutCustomPayload handshakePayload = new PacketPlayOutCustomPayload(new MinecraftKey("yplugin", "handshake_s2c"), new PacketDataSerializer(buf));
 
                     ByteBuf settingsbuffer = Unpooled.buffer();
                     String settings = YPlugin.getInstance().getServerSettingService().getServerSettingsAsString();
                     settingsbuffer.writeBytes(settings.getBytes(StandardCharsets.UTF_8));
-                    PacketPlayOutCustomPayload payload2 = new PacketPlayOutCustomPayload(new MinecraftKey("yplugin", "open_settings"), new PacketDataSerializer(settingsbuffer));
+                    PacketPlayOutCustomPayload openSettingsPayload = new PacketPlayOutCustomPayload(new MinecraftKey("yplugin", "open_settings"), new PacketDataSerializer(settingsbuffer));
 
-                    CraftPlayer craftPlayer = (CraftPlayer) player1;
-                    PlayerConnection b = craftPlayer.getHandle().c;
+                    NMSPlayer nmsPlayer = NMSUtil.getNMSPlayer(bukkitPlayer);
+                    nmsPlayer.sendPacket(handshakePayload);
+                    nmsPlayer.sendPacket(openSettingsPayload);
 
-                    b.a(payload);
-                    b.a(payload2);
-
-                    player1.sendMessage("§c§lYMod §r§7» §aYou have logged in with the master password!");
-                }else player1.sendMessage("§c§lYMod §r§7» §cIncorrect password! Please try again!");
+                    bukkitPlayer.sendMessage("§c§lYMod §r§7» §aYou have logged in with the master password!");
+                }else bukkitPlayer.sendMessage("§c§lYMod §r§7» §cIncorrect password! Please try again!");
                 break;
             case "hello":
-                String playerName = readBufAsString(byteBuf);
-                LoggingService.warning("YClient User:" + playerName + " has joined the server.");
                 break;
             case "update_settings":
                 String packetValue = readBufAsString(byteBuf);
